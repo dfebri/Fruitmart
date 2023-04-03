42e8d044f2b8617ea4aff61f8276f50ee1131b36
dfebri <dwifebrimurcahyo@gmail.com>
2023-04-03 23:57:27 +0700

    [initial commit]

---
+++ src/main/java/com/enigma/simpleproduct/SimpleproductApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.simpleproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleproductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/simpleproduct/SimpleproductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.simpleproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
