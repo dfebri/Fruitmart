04b0aecd232a362e4beeb1d468e26726ff14f113
dfebri <dwifebrimurcahyo@gmail.com>
2023-04-09 15:18:15 +0700

    [ADD] add Custom Respon

--- src/main/java/com/enigma/fruitmart/controller/CustomerController.java
+++ src/main/java/com/enigma/fruitmart/controller/CustomerController.java
@@ -2,7 +2,11 @@ package com.enigma.fruitmart.controller;
 
 import com.enigma.fruitmart.entitas.Customer;
 import com.enigma.fruitmart.service.CustomerService;
+import com.enigma.fruitmart.utils.customRespone.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,8 +23,14 @@ public class CustomerController {
     }
 
     @PostMapping
-    public Customer saveCustomer (@RequestBody Customer customer){
-        return customerService.saveCustomer(customer);
+    public ResponseEntity<Response<Customer>> saveCustomer (@RequestBody Customer customer){
+        String message = "Data customer has been inserted";
+        Response<Customer> response = new Response<>();
+        response.setMessage(message);
+        response.setData(customerService.saveCustomer(customer));
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(response);
     }
 
     @PutMapping
